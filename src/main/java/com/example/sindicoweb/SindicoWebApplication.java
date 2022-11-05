0f35c55f36616e96a12f2cd9166aa521b61fe0f4
Felipe Guimarães <54296382+felipe2g@users.noreply.github.com>
2022-11-04 21:17:03 -0300

    initial commit

---
+++ src/main/java/com/example/sindicoweb/SindicoWebApplication.java
@@ -0,0 +1,13 @@
+package com.example.sindicoweb;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SindicoWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SindicoWebApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/sindicoweb/SindicoWebApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.sindicoweb;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SindicoWebApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
